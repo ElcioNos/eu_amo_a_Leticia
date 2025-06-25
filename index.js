f84c3b3bf6b66d6974e419ffe3dcb06885329438
ElcioNos <elcionosjunior5461@gmail.com>
2025-06-25 15:10:53 -0300

    Add files via upload

---
+++ index.js
@@ -0,0 +1,13 @@
+let button = document.getElementById("button");
+button.addEventListener("click", mostrarMensagem);
+
+function mostrarMensagem() {
+    alert("EU SABIA!!! Você é uma pessoa incrível! (✿◠‿◠)");
+}
+
+let button2 = document.getElementById("button2");
+button2.addEventListener("click", mostrarMensagem2);
+
+function mostrarMensagem2() {
+    alert("É MENTIRA!!! E você continua sendo uma pessoa incrível! (✿◠‿◠)");
+}
